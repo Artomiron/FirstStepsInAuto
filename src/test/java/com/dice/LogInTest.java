1513ca9ed8d277cea7d945371801ce74f4138d28
Artomiron <redtom93@gmail.com>
2018-09-15 15:00:26 +0300

    created method for finding element on the page, input some date in the field and click some button

--- src/test/java/com/dice/LogInTest.java
+++ src/test/java/com/dice/LogInTest.java
@@ -1,12 +1,22 @@
 package com.dice;
 
+import base.BaseTest;
 import org.testng.annotations.Test;
+import pages.LogInPage;
 
-public class LogInTest {
+public class LogInTest extends BaseTest {
     @Test
     public void possitiveLoginTest(){
+        LogInPage loginPage = new LogInPage(driver);
+        loginPage.openLoginPage();
+
         // open authorization page
+
+        loginPage.fillUpEmailAndPassword("s2b.artem@gmail.com", "slim1984");
         // set field login and password
+
+
+        loginPage.pushSingingButton();
         // push the button login
         // varification
         // - user name correct
--- src/test/java/pages/LogInPage.java
+++ src/test/java/pages/LogInPage.java
@@ -1,11 +1,33 @@
 package pages;
 
 import base.BasePageObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LogInPage extends BasePageObject<LogInPage> {
-    protected LogInPage(WebDriver driver){
+
+    private static final String URL = "https://www.dice.com/dashboard/logout";
+
+    private By emailField = By.xpath("//input[@id='email']");
+    private By passwordField = By.xpath("//input[@id='password']");
+    private By singButton = By.xpath("//button[@type='submit']");
+
+    public LogInPage(WebDriver driver){
         super(driver);
         //toDo auto generated constructor stub
+    }
+
+    public void openLoginPage(){
+        getPage(URL);
+    }
+
+    public void fillUpEmailAndPassword(String email, String password){
+        type(email, emailField);
+        type(password, passwordField);
+    }
+
+    public ProfilePage pushSingingButton(){
+        click(singButton);
+        return new ProfilePage(driver);
     }
 }
